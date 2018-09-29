badc3fed49e79a628cf60230ce6022a5051def40
nileshv14 <nilesh.verma@ofbusiness.in>
2018-09-29 14:42:47 +0530

    separate function for executing tasks

--- src/main/java/TestTasks.java
+++ src/main/java/TestTasks.java
@@ -9,17 +9,7 @@ public class TestTasks {
 
     private static final Logger logger = Logger.getLogger(TestTasks.class.getName());
 
-    public static void main(String args[]) throws InterruptedException {
-
-        Scanner scanner = new Scanner(System.in);
-
-        logger.info("Please enter number of tasks to be executed");
-
-        int numberOfTasks = scanner.nextInt();
-
-        logger.info("Please enter number of threads");
-
-        int numberOfThreads = scanner.nextInt();
+    public static void RunTasks(int numberOfThreads, int numberOfTasks) {
 
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
 
@@ -50,4 +40,49 @@ public class TestTasks {
             e.printStackTrace();
         }
     }
+
+    public static void main(String args[]) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        logger.info("Please enter number of tasks to be executed");
+
+        int numberOfTasks = scanner.nextInt();
+
+        logger.info("Please enter number of threads");
+
+        int numberOfThreads = scanner.nextInt();
+
+        RunTasks(numberOfThreads, numberOfTasks);
+    }
 }
+
+
+ /*ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        final CountDownLatch countDownLatch = new CountDownLatch(numberOfTasks - 2);
+
+        for (int i = 0; i < numberOfTasks - 1; i++) {
+
+            if (i == 0) {
+
+                logger.info("Executing first task " + (i + 1));
+
+                logger.info("execution of task 1 finished ");
+            } else {
+
+                Runnable runTask = new ExecuteTasks(String.valueOf(i + 1), countDownLatch);
+
+                executorService.execute(runTask);
+            }
+        }
+
+        try {
+            countDownLatch.await();
+            TimeUnit.MILLISECONDS.sleep(1000);
+            Runnable runTask = new ExecuteTasks(String.valueOf(numberOfTasks), countDownLatch);
+            executorService.execute(runTask);
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }*/
